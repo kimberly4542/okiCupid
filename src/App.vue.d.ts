7bc8ad0471d3b7ff9ac4414dd5adb12420c6f6eb
kimberly4542 <kimberlymagallen122800@gmail.com>
2024-05-10 11:28:30 +0800

    adding other file for appvue

---
+++ src/App.vue.d.ts
@@ -0,0 +1,8 @@
+// App.vue.d.ts
+import { DefineComponent } from 'vue';
+
+// Define the type of the component
+declare const App: DefineComponent<{}, {}, unknown>;
+
+// Export the component type
+export default App;
